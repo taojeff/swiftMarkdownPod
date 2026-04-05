652fc15b54c7fd641fd83fb39b78fdfd503ee20e
taojeff <990073834@qq.com>
2026-04-05 21:29:43 +0800

    Update CommonMarkConverter to use cmark_gfm
    Replaced swiftCmarkPod with cmark_gfm and cmark_gfm_extensions.

--- Sources/Markdown/Parser/CommonMarkConverter.swift
+++ Sources/Markdown/Parser/CommonMarkConverter.swift
@@ -7,7 +7,8 @@
  See https://swift.org/LICENSE.txt for license information
  See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 */
-import swiftCmarkPod
+import cmark_gfm
+import cmark_gfm_extensions
 /** 用swift_cmark_pod替代 cmark_gfm 和 cmark_gfm_extensions
 import cmark_gfm
 import cmark_gfm_extensions
